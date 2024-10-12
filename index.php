32ac6c287311442d6c358c58caaac4db30ee36a7
abdelilah <abdelilahghoummach21@gmail.com>
2024-10-12 23:07:26 +0100

    the home page it's the first in the portfolio

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <title>Abdelilah Ghoummach</title>
+ <link rel="stylesheet" href="css/style.css">
+ <link rel="icon" href="icon/me.jpg">
+ <link rel="stylesheet" href="https://fontsgoogleapis.com/css2?family=Josefin+Sans:wght@100;200;300;400;500;600;700&display=swap">
+</head>
+<body>
+<section class="home">
+      <nav>
+           <h2>Abdelilah <span>Online</span></h2>
+           <ul>
+               <li><a href="#home">Home</a></li>
+               <li><a href="#home">About Me</a></li>
+               <li><a href="#home">Services</a></li>
+               <li><a href="#home">Skills</a></li>
+               <li><a href="#home">Contact Me</a></li>
+           </ul>
+           <a href="" class="btn">Subscribe</a>
+      </nav>
+      <div class="content">
+             <div class="container-texts">
+                   <h4>Hello, my name is</h4>
+                   <h1>Abdelilah <span>Ghoummach</span> </h1>
+                   <h3>I'm a <span>Full-stack </span> devloper</h3>
+                   <a href="#" class="btn">Download cv</a>
+                   <div class="socila">
+                        <a href="https://www.instagram.com/abdel_ilah_ghoummach?igsh=YnEzOXN6bnlyZ3Rq" target="_blank"><img src="img/instagram.png" class="imgs" alt=""></a>
+                        <a href="https://www.facebook.com/abdelilah.ghammach.1/" target="_blank"><img src="img/facebook.png" class="imgs" alt=""></a>
+                        <a href="https://telegram.org/dl" target="_blank"><img src="img/telegram.png" class="imgs" alt=""></a>
+                        <a href="https://web.whatsapp.com/" target="_blank"><img src="img/wathsapp.png" class="imgs" alt=""></a>
+                   </div>
+             </div>
+             <div class="container-img">
+                  
+                  <img src="img/shape2.png" alt="" class="shape">
+                  <img src="img/pic.png" alt="" class="mypic">
+             </div>
+      </div>
+</section>
+<section class="about">
+
+</body>
+</html>
